bd215ad19a384889d07bcae2c5faad10ba600b59
Ezlan Zulfiqree <87627696+ezdigix@users.noreply.github.com>
2021-08-03 00:01:56 +0800

    commit

---
+++ src/components/AppBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-toolbar app color="yellow" flat fixed>
+    <v-img
+      src="https://imgr.search.brave.com/G0Jgtgb5xuUrtw9WuD3vcqX3OmwjVlANrow3nc8aDuY/fit/387/140/no/1/aHR0cDovL2ltZzEu/d2lraWEubm9jb29r/aWUubmV0L19fY2Iy/MDEzMDkyNzEzMzQx/OC9wb2tlbW9uL2Zy/L2ltYWdlcy8xLzFi/L1Bva2VkZXhfbG9n/by5wbmc"
+      contain
+      max-height="40"
+    />
+  </v-toolbar>
+</template>
